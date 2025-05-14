f09c0f91e7c941bbc8dc4452095cd245b2ea2dc0
jordan te ka <jordanteka256@gmail.com>
2025-05-14 13:56:33 +0300

    Add files via upload

---
+++ FeedBackForm/Controllers/FeedBackController.cs
@@ -0,0 +1,25 @@
+﻿using FeedBackForm.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeedBackForm.Controllers
+{
+    public class FeedBackController : Controller
+    {
+        public IActionResult Submit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Submit(FeedBack model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "Feedback submitted successfully!";
+                return View(model);
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ FeedBackForm/Models/FeedBack.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FeedBackForm.Models
+{
+    public class FeedBack
+    {
+        [Required]
+        public int Customer_ID { get; set; }
+
+        [Required]
+        public string Customer_Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Enter a valid email address")]
+        public string Email_Address { get; set; }
+
+        [Required]
+        [Phone(ErrorMessage = "Enter a valid mobile number")]
+        public string Mobile_No { get; set; }
+
+        [Required]
+        [StringLength(300, ErrorMessage = "Comment cannot exceed 300 characters")]
+        public string Comment { get; set; }
+    }
+}
