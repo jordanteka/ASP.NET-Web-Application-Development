280c6f7ea6f18361addca45cebbd8e7c03f3deb4
jordan te ka <jordanteka256@gmail.com>
2025-05-14 14:21:14 +0300

    Add files via upload

---
+++ list box/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace List_box.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
