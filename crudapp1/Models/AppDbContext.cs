29ae9184e5793b5915f7c8124f2b20cd15a86694
jordan te ka <jordanteka256@gmail.com>
2025-05-14 13:44:27 +0300

    Add files via upload

---
+++ crudapp1/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudApp.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ crudapp1/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CrudApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Author is required")]
+        public string Author { get; set; }
+
+        [Range(1000, 2100, ErrorMessage = "Enter a valid year")]
+        public int Year { get; set; }
+    }
+}
