1b8d883f24c038dfa710dd8684645b2386e1c1f7
jordan te ka <jordanteka256@gmail.com>
2025-05-14 14:27:35 +0300

    Add files via upload

---
+++ student Feedback website/Controllers/FeedBackController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CustomerFeedBackWebsite.Models;
+
+namespace CustomerFeedBackWebsite.Controllers
+{
+    public class FeedBackController : Controller
+    {
+        private static List<FeedBack> feedbackList = new List<FeedBack>();
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(FeedBack feedback)
+        {
+            feedbackList.Add(feedback);
+            ViewBag.Message = "Feedback submitted successfully";
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View(feedbackList);
+        }
+    }
+    }
---
+++ student Feedback website/Models/FeedBack.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomerFeedBackWebsite.Models
+{
+    public class FeedBack
+    {
+        public int Customer_ID { get; set; }
+        public string Customer_Name { get; set; }
+        public string Email_Address { get; set; }
+        public string Mobile_No { get; set; }
+        public string Comment { get; set; }
+    }
+}
