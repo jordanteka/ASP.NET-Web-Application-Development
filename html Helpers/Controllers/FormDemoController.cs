2f371ac162919c7e7785edb8874377da9c099878
jordan te ka <jordanteka256@gmail.com>
2025-05-14 14:11:34 +0300

    Add files via upload

---
+++ html Helpers/Controllers/FormDemoController.cs
@@ -0,0 +1,29 @@
+﻿using htmlhelpers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace htmlhelpers.Controllers
+{
+    public class FormDemoController : Controller
+    {
+        public IActionResult ShowForm()
+        {
+            ViewBag.CountryList = new List<string> { "Nigeria", "Kenya", "USA", "UK" };
+            ViewBag.Languages = new List<string> { "English", "French", "Swahili", "Spanish" };
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ShowForm(FormDemo model)
+        {
+            ViewBag.CountryList = new List<string> { "Nigeria", "Kenya", "USA", "UK" };
+            ViewBag.Languages = new List<string> { "English", "French", "Swahili", "Spanish" };
+
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "Form submitted successfully!";
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ html Helpers/Models/FormDemo.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace htmlhelpers.Models
+{
+    public class FormDemo
+    {
+        [Display(Name = "Full Name")]
+        public string Name { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string HiddenInfo { get; set; }
+
+        public bool AgreeToTerms { get; set; }
+
+        public string Gender { get; set; }
+
+        [Display(Name = "Country")]
+        public string SelectedCountry { get; set; }
+
+        public List<string> SelectedLanguages { get; set; }
+
+        public string Comments { get; set; }
+
+        [Display(Name = "Email Address")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
