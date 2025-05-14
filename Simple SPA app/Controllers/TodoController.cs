e4dd7608eac364158f7dc27d38a2d17453dfae99
jordan te ka <jordanteka256@gmail.com>
2025-05-14 14:21:32 +0300

    Add files via upload

---
+++ Simple SPA app/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleSPAApp.Models;
+
+namespace SimpleSPAApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        private static List<TodoItem> todos = new List<TodoItem>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult GetAll()
+        {
+            return Json(todos);
+        }
+
+        [HttpPost]
+        public JsonResult Add([FromBody] TodoItem item)
+        {
+            item.Id = todos.Count + 1;
+            todos.Add(item);
+            return Json(new { success = true, message = "Task added", item });
+        }
+    }
+}
---
+++ Simple SPA app/Models/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleSPAApp.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
